7bbb0db9c48c6ee0dae5b673f555954821d34a0e
Rafa Gómez <rgomezcasas@gmail.com>
2020-03-23 15:30:04 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import * as core from '@actions/core'
+import * as yaml from 'js-yaml'
+
 
 async function run(): Promise<void> {
   try {
     // const githubToken: string = core.getInput('GITHUB_TOKEN')
-    const mapping: string     = core.getInput('mapping')
+    const mapping = yaml.safeLoad(core.getInput('mapping'))
 
     core.error(`some message`)
     core.error(`The mapping is: ${mapping}`)
