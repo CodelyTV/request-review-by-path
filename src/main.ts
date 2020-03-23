3ff509c973387a92f6755546b975be72d9add07d
Rafa Gómez <rgomezcasas@gmail.com>
2020-03-23 15:54:15 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
-import * as core from '@actions/core'
-import * as yaml from 'js-yaml'
-
+import * as core   from '@actions/core'
+import * as github from '@actions/github'
+import * as yaml   from 'js-yaml'
 
 async function run(): Promise<void> {
   try {
-    // const githubToken: string = core.getInput('GITHUB_TOKEN')
     const mapping = yaml.safeLoad(core.getInput('mapping'))
+    const octokit = new github.GitHub(core.getInput('GITHUB_TOKEN'))
 
-    core.error(`Starting`)
-    for (let user in mapping) {
+    const repo = github.context.repo
+
+    const modifiedFiles = await octokit.repos.getCommit({
+      owner: repo.owner,
+      repo: repo.repo,
+      ref: github.context.ref
+    })
+
+    core.error(`Starting modified files:`)
+    console.log(modifiedFiles)
+    for (const user in mapping) {
       const paths = mapping[user]
 
       core.error(`The user "${user}" has the paths: ${paths}`)
