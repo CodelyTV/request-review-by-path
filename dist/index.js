770c2bb615c03bfa0a072731494af6c8204c8d70
Rafa Gómez <rgomezcasas@gmail.com>
2020-03-23 16:04:44 +0100

    wip

--- dist/index.js
+++ dist/index.js
@@ -3509,12 +3509,15 @@ function run() {
         try {
             const mapping = yaml.safeLoad(core.getInput('mapping'));
             const octokit = new github.GitHub(core.getInput('GITHUB_TOKEN'));
-            const repo = github.context.repo;
             const modifiedFiles = yield octokit.repos.getCommit({
-                owner: repo.owner,
-                repo: repo.repo,
-                ref: github.context.ref
+                owner: github.context.repo.owner,
+                repo: github.context.repo.repo,
+                ref: github.context.sha
             });
+            core.error(`SHA`);
+            console.log(github.context.sha);
+            core.error(`TOH EL CONTEXT`);
+            console.log(github.context);
             core.error(`Starting modified files:`);
             console.log(modifiedFiles);
             for (const user in mapping) {
