3a7b31cb7f81ca6f7f9e36ee9ea2de91d7f6d8f2
Rafa Gómez <rgomezcasas@gmail.com>
2020-03-23 15:03:41 +0100

    wip

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import * as core from '@actions/core'
 
 async function run(): Promise<void> {
   try {
-    const githubToken: string = core.getInput('GITHUB_TOKEN')
+    // const githubToken: string = core.getInput('GITHUB_TOKEN')
     const mapping: string     = core.getInput('mapping')
 
     core.debug(`The mapping is: ${mapping}`)
-
-    core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     core.setFailed(error.message)
   }
